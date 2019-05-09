165850ea457a071345212e74354c5fa78c2eaf63
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-05-09 16:29:53 +0600

    Login-signup pages

--- posts/forms.py
+++ posts/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-class AuthenticationForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'autofocus': True}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class SignupForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
--- posts/views.py
+++ posts/views.py
@@ -1,36 +1,45 @@
 from django.shortcuts import render, redirect
-from .forms import AuthenticationForm
+from .forms import LoginForm, SignupForm
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
 from .models import Post
 from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 def home_view(request):
-    auth_form = AuthenticationForm()
-    signup_form = UserCreationForm()
+    auth_form = LoginForm()
+    signup_form = SignupForm()
     return render(request, "home.html", {"auth_form": auth_form, "signup_form": signup_form})
 
 
 def user_login(request):
     if request.method == "POST":
-        form = AuthenticationForm(request.POST)
+        form = LoginForm(request.POST)
         if form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-        return redirect('/')
+                return redirect('/')
+        return render(request, "login.html", {"form": form, "errors": "Invalid username/Password"})
+
+    elif request.method == "GET":
+        form = LoginForm()
+        return render(request, "login.html", {"form": form})
 
 
 def user_create(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/')
+        return render(request, "signup.html", {"form": form})
+
+    elif request.method == "GET":
+        form = SignupForm()
+        return render(request, "signup.html", {"form": form})
 
 
 @csrf_exempt
